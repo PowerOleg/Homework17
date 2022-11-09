5449b3e74939dade592d06036f7294731080ae20
PowerOleg <geraskin.oleg@mail.ru>
2022-11-09 18:33:47 +0500

    Searching added

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,20 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
-
 public class Main {
-
     public static void show(List arrayList) {
         System.out.println("The order list:");
         for (int i = 0; i < arrayList.size(); i++) {
             System.out.printf("%d. %s\n", i+1, arrayList.get(i));
         }
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<String> arrayList = new ArrayList<>();
 
         while (true) {
-            System.out.println("Enter an operation number or \"end\". 1-add, 2-show, 3-remove ");
+            System.out.println("Enter an operation number or \"end\". 1-add, 2-show, 3-remove, 4-search");
             System.out.print(">>");
             String s = scanner.nextLine();
             if (s.toLowerCase().equals("end")) break;
@@ -40,12 +38,21 @@ public class Main {
                         int i = Integer.parseInt(remove);
                         arrayList.remove(i-1);
                         continue;
-                    } catch (Exception e) {}
+                    } catch (Exception e) {}                    //?пустой catch
                     arrayList.remove(remove);
                 break;
+                case "4":
+                    System.out.println("Input a word to search");
+                    String search = scanner.nextLine();
+                    for (int i = 0; i < arrayList.size(); i++) {
+                        if (arrayList.get(i).toLowerCase().contains(search.toLowerCase())) {
+                            System.out.printf("%d. %s\n", i+1, arrayList.get(i));
+                        }
+//                        добавить что если ничего не найдено то ничего не найдено
+//                        добавить кнопку возврата в главное меню
+                    }
+                    break;
             }
-
         }
-
     }
 }

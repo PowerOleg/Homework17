82dc90b1be9dce96e9a393a85c3ca966b5d6f12b
PowerOleg <geraskin.oleg@mail.ru>
2022-11-09 17:22:34 +0500

    end button, new show method

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,23 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void show(List arrayList) {
+        System.out.println("The order list:");
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.printf("%d. %s\n", i+1, arrayList.get(i));
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<String> arrayList = new ArrayList<>();
 
         while (true) {
-            System.out.println("Enter an operation number. 1-add, 2-show, 3-remove");
+            System.out.println("Enter an operation number or \"end\". 1-add, 2-show, 3-remove ");
             System.out.print(">>");
             String s = scanner.nextLine();
-//добавить завершение при команде end
+            if (s.toLowerCase().equals("end")) break;
+
             switch (s) {
             case "1":
                 System.out.println("What product would you like to add?");
@@ -22,19 +30,11 @@ public class Main {
                 System.out.printf("You made purchases: %d\n", arrayList.size());
             break;
             case "2":
-                System.out.println("The order list:");
-                for (int i = 0; i < arrayList.size(); i++) {
-                    System.out.printf("%d. %s\n", i+1, arrayList.get(i));
-                }
+                show(arrayList);
             break;
-              //начало повторяется как в case 2 - сделать без повторений
             case "3":
-                System.out.println("The order list:");
-                for (int i = 0; i < arrayList.size(); i++) {
-                    System.out.printf("%d. %s\n", i+1, arrayList.get(i));
-                }
+                show(arrayList);
                 System.out.println("Which product would you like to remove? Input number or name?");
-                //доделать удаление по имени
                 String remove = scanner.nextLine();
                     try {
                         int i = Integer.parseInt(remove);
@@ -44,7 +44,6 @@ public class Main {
                     arrayList.remove(remove);
                 break;
             }
-
 
         }
 

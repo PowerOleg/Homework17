d6ecef2368289b206783953dabd6b3f53b9083e4
PowerOleg <geraskin.oleg@mail.ru>
2022-11-23 11:55:32 +0500

    ^\d+

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,17 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Main {
     public static <T> void show(List<T> arrayList) {
         System.out.println("The order list:");
         for (int i = 0; i < arrayList.size(); i++) {
             System.out.printf("%d. %s\n", i+1, arrayList.get(i));
         }
+    }
+    public static boolean isNumber(String value) {
+        return Pattern.compile("^\\d+$").matcher(value).find();
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -20,32 +25,32 @@ public class Main {
             if (s.toLowerCase().equals("end")) break;
 
             switch (s) {
-            case "1":
-                System.out.println("Enter product's name which you would like to add or input \"return\"");
-                System.out.print(">>");
-                String ss = scanner.nextLine();
-                if (ss.toLowerCase().equals("return")) break;
-                arrayList.add(ss);
-                System.out.printf("You made purchases: %d\n", arrayList.size());
-            break;
-            case "2":
-                show(arrayList);
-            break;
-            case "3":
-                show(arrayList);
-                System.out.println("Enter product's number or name you would like to remove or input \"return\"");
-                String remove = scanner.nextLine();
-                if (remove.toLowerCase().equals("return")) break;
-                try {
-                        int i = Integer.parseInt(remove);
-                        arrayList.remove(i - 1);
+                case "1":
+                    System.out.println("Enter product's name which you would like to add or input \"return\"");
+                    System.out.print(">>");
+                    String ss = scanner.nextLine();
+                    if (ss.toLowerCase().equals("return")) break;
+                    arrayList.add(ss);
+                    System.out.printf("You made purchases: %d\n", arrayList.size());
+                    break;
+                case "2":
+                    show(arrayList);
+                    break;
+                case "3":
+                    show(arrayList);
+                    System.out.println("Enter product's number or name you would like to remove or input \"return\"");
+                    String remove = scanner.nextLine();
+                    if (remove.toLowerCase().equals("return")) break;
+
+                    if (arrayList.remove(remove)) {
                         System.out.println("The element removed");
-                        continue;
-                    } catch (Exception e) {}
-                if (arrayList.remove(remove)) {
-                    System.out.println("The element removed");
-                } else System.out.println("Sorry, no matches");
-                break;
+                    } else if (isNumber(remove)) {
+                        int i = Integer.parseInt(remove);
+                        arrayList.remove(--i);
+                        System.out.println("The element removed");
+                    } else System.out.println("Sorry, no matches");
+
+                    break;
                 case "4":
                     System.out.println("Input a word to search or input \"return\"");
                     String search = scanner.nextLine();
